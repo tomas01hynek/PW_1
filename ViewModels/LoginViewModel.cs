f0e715a610c6d62425fa98a8501851c90f47b4b8
tomas01hynek <154077188+tomas01hynek@users.noreply.github.com>
2023-12-20 13:33:56 +0100

    Add files via upload
    2.kontrola-ViewModels

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace UTB.Eshop.Application.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string? Username { get; set; }
+        [Required]
+        public string? Password { get; set; }
+        public bool LoginFailed { get; set; }
+    }
+}
